65acf3dbc612aab668ad81bcff91aded7a906e3a
Pierre Cahuzac <cahuzac.p@gmail.com>
2022-09-28 21:53:26 +0200

    grid responsive

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,4 +1,6 @@
-import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
+import { GiConsoleController } from "react-icons/gi";
+
 import "../assets/CSS/grid.css";
 const Grid = ({
   color,
@@ -31,13 +33,33 @@ const Grid = ({
     numberOfCols.push(i);
   }
 
+  const [widthOfPixel, setWidthOfPixel] = useState("20px");
+  const [heightOfPixel, setHeightOfPixel] = useState("150px");
+  const [widthGrid, setWidthGrid] = useState();
+
+  const test = window.innerWidth / numberOfCols.length;
+  /* console.log(widthGrid); */
+  console.log(numberOfRows.length);
+  console.log("test", test);
+  const newtest = window.innerWidth;
+
+  let widthOfGridElement = document.getElementsByClassName("grid");
+
+  console.log("widthOfGridElement.inn", widthOfGridElement.innerWidth);
+
   return (
     <div className="grid_container">
-      <div className="grid">
+      <div className="grid" style={{ height: newtest }}>
         {numberOfRows.map((row, idRow) => (
-          <div className="grid_row" key={row} name={row}>
+          <div
+            className="grid_row"
+            key={row}
+            name={row}
+            style={{ height: test }}
+          >
             {numberOfCols.map((pixel, idCol) => (
               <div
+                style={{ width: test, height: test }}
                 className="grid_pixel"
                 id={idRow.toString() + "-" + idCol.toString()}
                 key={pixel}
